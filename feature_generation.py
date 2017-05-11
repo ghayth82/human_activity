9911fa43e2b187c027980758f811c0f8cd2c14bf
Rodrigo Rezende <rarezende1@gmail.com>
2017-05-10 22:38:51 -0400

    Included filtered signal

--- feature_generation.py
+++ feature_generation.py
@@ -1,81 +1,235 @@
 # -----------------------------------------------------------------
 # Data preprocessing for human activity recognition
+# -----------------------------------------------------------------
+
+#%% -----------------------------------------------------------------
+# Definition of Butterworth filters
+
+from scipy import signal
+
+def butter_highpass_filter(signal_data, cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='highpass', analog=False)
+    filtered_signal = signal.filtfilt(b, a, signal_data)
+    return filtered_signal
+
+def butter_lowpass_filter(signal_data, cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='lowpass', analog=False)
+    filtered_signal = signal.filtfilt(b, a, signal_data)
+    return filtered_signal
+
+def butter_bandpass_filter(signal_data, lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = signal.butter(order, [low, high], btype='bandpass', analog=False)
+    filtered_signal = signal.filtfilt(b, a, signal_data)
+    return filtered_signal
+
+#%% ----------------------------------------------------------------
+# Feature generation
+
+import pandas as pd
+
+activities = {
+                "Jogging"  : 0, 
+                "LyingDown": 1,
+                "Sitting"  : 2,
+                "Stairs"   : 3,
+                "Standing" : 4,
+                "Walking"  : 5
+              }		 
+
+feat_list = [
+                'id'           ,
+                'activity_id'  ,
+                'win_begin_idx',
+                'win_end_idx'  ,
+                'median_x'     ,
+                'median_y'     ,
+                'median_z'     ,
+                'median_mag'   ,
+                'q25_x'        ,
+                'q25_y'        ,
+                'q25_z'        ,
+                'q25_mag'      ,
+                'q75_x'        ,
+                'q75_y'        ,
+                'q75_z'        ,
+                'q75_mag'      ,
+                'mean_x'       ,
+                'mean_y'       ,
+                'mean_z'       ,
+                'mean_mag'     ,
+                'mean_filt_x'  , 
+                'mean_filt_y'  , 
+                'mean_filt_z'  , 
+                'std_x'        ,
+                'std_y'        ,
+                'std_z'        ,
+                'std_mag'      ,
+                'std_filt_x'   , 
+                'std_filt_y'   , 
+                'std_filt_z'   , 
+            ]
+
+def generate_features(window_data):
+    window_size = len(window_data)
+    
+    # sets user_id to id with most rows in the window
+    grouped = window_data.groupby('id').size()
+    user_id = grouped.sort_values(ascending=False).index[0]
+    
+    # sets activity_id to activity with most rows in the window
+    grouped = window_data.groupby('activity').size()
+    activity = grouped.sort_values(ascending=False).index[0]
+    activity_id = activities[activity]
+    
+    features = {
+                'id'               : user_id,
+                'activity_id'      : activity_id,
+                'win_begin_idx'    : window_data.iloc[0,:].name, 
+                'win_end_idx'      : window_data.iloc[window_size-1,:].name,
+                'median_x'         : window_data['x'].median(),
+                'median_y'         : window_data['y'].median(),
+                'median_z'         : window_data['z'].median(),
+                'median_mag'       : window_data['magnitude'].median(),
+                'q25_x'            : window_data['x'].quantile(0.25),
+                'q25_y'            : window_data['y'].quantile(0.25),
+                'q25_z'            : window_data['z'].quantile(0.25),
+                'q25_mag'          : window_data['magnitude'].quantile(0.25),
+                'q75_x'            : window_data['x'].quantile(0.75),
+                'q75_y'            : window_data['y'].quantile(0.75),
+                'q75_z'            : window_data['z'].quantile(0.75),
+                'q75_mag'          : window_data['magnitude'].quantile(0.75),
+                'mean_x'           : window_data['x'].mean(), 
+                'mean_y'           : window_data['y'].mean(), 
+                'mean_z'           : window_data['z'].mean(), 
+                'mean_mag'         : window_data['magnitude'].mean(), 
+                'mean_filt_x'      : window_data['filt_x'].mean(), 
+                'mean_filt_y'      : window_data['filt_y'].mean(), 
+                'mean_filt_z'      : window_data['filt_z'].mean(), 
+                'std_x'            : window_data['x'].std(), 
+                'std_y'            : window_data['y'].std(), 
+                'std_z'            : window_data['z'].std(), 
+                'std_mag'          : window_data['magnitude'].std(),
+                'std_filt_x'       : window_data['filt_x'].std(), 
+                'std_filt_y'       : window_data['filt_y'].std(), 
+                'std_filt_z'       : window_data['filt_z'].std(), 
+			     }
+    
+    return features
+
+
+def generate_pure_samples(train_data, window_size):
+    user_ids = train_data.id.unique()
+    features = []
+    for user_id in user_ids:
+        user_data = train_data[train_data.id == user_id]
+        for activity in activities.keys():
+            activity_data = user_data[user_data.activity == activity]
+            begin_row = 0
+            end_row = window_size
+            while(end_row <= activity_data.shape[0]):
+                window_data = activity_data[begin_row:end_row]
+                features.append(generate_features(window_data))
+                begin_row = end_row
+                end_row = end_row + window_size
+                    
+    samples = pd.DataFrame(features, columns = feat_list)
+
+    return samples
+    
+
+def generate_hybrid_samples(train_data, window_size):
+    features = []
+    begin_row = 0
+    end_row = window_size
+    while(end_row <= train_data.shape[0]):
+        window_data = train_data[begin_row:end_row]
+        features.append(generate_features(window_data))
+        begin_row = end_row
+        end_row = end_row + window_size
+                    
+    samples = pd.DataFrame(features, columns = feat_list)
+
+    return samples
+
+
+def preprocess_train_data(train_raw, sampling_rate):
+    train_data = train_raw.drop_duplicates().copy() 
+    
+    train_data['idx'] = list(range(len(train_data)))
+    train_data = train_data.set_index('idx')
+    
+    outliers = train_data['x'] > train_data['x'].quantile(0.999)
+    train_data.loc[outliers, 'x'] = train_data['x'].quantile(0.999)
+    outliers = train_data['x'] < train_data['x'].quantile(0.001)
+    train_data.loc[outliers, 'x'] = train_data['x'].quantile(0.001)   
+    
+    outliers = train_data['y'] > train_data['y'].quantile(0.999)
+    train_data.loc[outliers, 'y'] = train_data['y'].quantile(0.999)
+    outliers = train_data['y'] < train_data['y'].quantile(0.001)
+    train_data.loc[outliers, 'y'] = train_data['y'].quantile(0.001)    
+
+    outliers = train_data['z'] > train_data['z'].quantile(0.999)
+    train_data.loc[outliers, 'z'] = train_data['z'].quantile(0.999)
+    outliers = train_data['z'] < train_data['z'].quantile(0.001)
+    train_data.loc[outliers, 'z'] = train_data['z'].quantile(0.001)    
+    
+    train_data['magnitude'] = (train_data['x']**2 + train_data['y']**2 + train_data['z']**2)**0.5
+    
+    '''              
+    train_data['filt_x'] = butter_lowpass_filter(train_data['x'], 0.25, sampling_rate)
+    train_data['filt_y'] = butter_lowpass_filter(train_data['y'], 0.25, sampling_rate)
+    train_data['filt_z'] = butter_lowpass_filter(train_data['z'], 0.25, sampling_rate)
+
+    train_data['filt_x'] = butter_highpass_filter(train_data['x'], 0.25, sampling_rate)
+    train_data['filt_y'] = butter_highpass_filter(train_data['y'], 0.25, sampling_rate)
+    train_data['filt_z'] = butter_highpass_filter(train_data['z'], 0.25, sampling_rate)
+    '''
+    train_data['filt_x'] = butter_bandpass_filter(train_data['x'], 1.25, 2.50, sampling_rate)
+    train_data['filt_y'] = butter_bandpass_filter(train_data['y'], 1.25, 2.50, sampling_rate)
+    train_data['filt_z'] = butter_bandpass_filter(train_data['z'], 1.25, 2.50, sampling_rate)
+    
+    return train_data
+
+    
+
 
 #%%
 import time
-import pandas as pd
+#import pandas as pd
 
 start_time = time.time()
 
 root_dir = "C:/Users/rarez/Documents/Data Science/human_activity/data/"
 
-train_raw = pd.read_csv(root_dir + "train_raw.csv")    
-train_raw = train_raw.drop_duplicates() 
-train_raw.loc[:, 'idx'] = list(range(len(train_raw)))
-train_raw = train_raw.set_index('idx')
-
-train_raw['x'] = butter_highpass_filter(train_raw['x'], 1, 20)
-train_raw['y'] = butter_highpass_filter(train_raw['y'], 1, 20)
-train_raw['z'] = butter_highpass_filter(train_raw['z'], 1, 20)
-
-train_raw.loc[:,'magnitude'] = (train_raw['x']**2 + train_raw['y']**2 + train_raw['z']**2)**0.5
+train_raw = pd.read_csv(root_dir + "train_raw.csv")   
 
 sampling_rate = 20 # Hz
-window_length = 2.5 # seconds
+window_length = 2 # seconds
 window_size = int(sampling_rate * window_length)
 
-activities = [{"act_type": "Jogging"    , "act_id": 0},
-              {"act_type": "LyingDown"  , "act_id": 1},
-              {"act_type": "Sitting"    , "act_id": 2},
-              {"act_type": "Stairs"     , "act_id": 3},
-              {"act_type": "Standing"   , "act_id": 4},
-              {"act_type": "Walking"    , "act_id": 5}]
+train_data = preprocess_train_data(train_raw, sampling_rate) 
 
-user_ids = train_raw.id.unique()
+train = generate_hybrid_samples(train_data, window_size)
 
-columns = ['id', 'win_begin_idx', 'win_end_idx', 'activity_id',
-           'median_x', 'mean_x', 'std_x', 
-           'median_y', 'mean_y', 'std_y', 
-           'median_z', 'mean_z', 'std_z', 
-           'median_mag', 'mean_mag', 'std_mag']
-
-features = []
-for user_id in user_ids:
-    user_data = train_raw[train_raw.id == user_id]
-    for activity in activities:
-        activity_data = user_data[user_data.activity == activity['act_type']]
-        begin_row = 0
-        end_row = window_size
-        while(end_row <= activity_data.shape[0]):
-            window_data = activity_data[begin_row:end_row]
-            
-            features.append({'id'               : user_id,
-                             'win_begin_idx'    : window_data.iloc[0,:].name, 
-                             'win_end_idx'      : window_data.iloc[window_size-1,:].name,
-                             'activity_id'      : activity['act_id'],
-                             'median_x'         : window_data['x'].median(),
-                             'mean_x'           : window_data['x'].mean(), 
-                             'std_x'            : window_data['x'].std(), 
-                             'median_y'         : window_data['y'].median(),
-                             'mean_y'           : window_data['y'].mean(), 
-                             'std_y'            : window_data['y'].std(), 
-                             'median_z'         : window_data['z'].median(),
-                             'mean_z'           : window_data['z'].mean(), 
-                             'std_z'            : window_data['z'].std(), 
-                             'median_mag'       : window_data['magnitude'].median(),
-                             'mean_mag'         : window_data['magnitude'].mean(), 
-                             'std_mag'          : window_data['magnitude'].std()})
-
-            begin_row = end_row
-            end_row = end_row + window_size
-                
-train = pd.DataFrame(features, columns = columns)
 train.to_csv(root_dir + 'train.csv', index = False)
 
 print("Total processing time: {:.2f} minutes".format((time.time()-start_time)/60))
 
+
+
+
                 
 #%%
+# ---------------------------------------------------------------------------
+# Filter tests
+
 import numpy as np
 import pandas as pd
 from scipy import signal
@@ -90,17 +244,6 @@ def sine_generator(fs, sinefreq, duration):
     result = pd.DataFrame({ 
         'data' : y_sine} ,index=t_sine)
     return result
-
-def butter_highpass(cutoff, fs, order=5):
-    nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
-    return b, a
-
-def butter_highpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_highpass(cutoff, fs, order=order)
-    y = signal.filtfilt(b, a, data)
-    return y
 
 fps = 30
 sine_fq = 10 #Hz
@@ -124,18 +267,46 @@ plt.title('filtered signal')
 plt.show()
 #%%
 
-sine = train_raw['x'][:200]
-filtered_sine = butter_highpass_filter(sine, 1, 20)
+import matplotlib.pyplot as plt
 
+train_data = preprocess_train_data(train_raw)
+
+user_id = 358
+activity = 'Stairs'
+user_data = train_data[train_data.id == user_id]
+activity_data = user_data[user_data.activity == activity]
+signal_data = activity_data['y'][:400]
+
+filtered_signal = butter_lowpass_filter(signal_data, 0.25, sampling_rate)
+#filtered_signal = butter_highpass_filter(signal_data, 0.5, sampling_rate)
+#filtered_signal = butter_bandpass_filter(signal_data, 0.5, 2.5, sampling_rate)
+         
 
 plt.figure(figsize=(10,10))
 plt.subplot(211)
-plt.plot(range(len(sine)),sine)
+plt.plot(range(len(signal_data)),signal_data)
 plt.title('generated signal')
 plt.subplot(212)
-plt.plot(range(len(filtered_sine)),filtered_sine)
+plt.plot(range(len(filtered_signal)),filtered_signal)
 plt.title('filtered signal')
 plt.show()
+
+
+outliers = abs(train_data[337540:337560]['x']) < 1e-6
+train_data.loc[outliers, 'x'] = 0
+
+outliers = train_raw['timestamp'] < 0
+
+train_raw[outliers]
+
+
+
+
+
+
+
+
+
 
 
 
--- model_training.py
+++ model_training.py
@@ -1,13 +1,14 @@
 # -----------------------------------------------------------------
 # Model training for human activity recognition
+# -----------------------------------------------------------------
 
+#%% ------------------------------------------------------------------------
 import time
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-#from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score, make_scorer
-from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
 
 start_time = time.time()
 
@@ -36,8 +37,23 @@ scores = np.array(scores)
 print("Accuracy: {:.3f} (+/- {:.3f})".format(scores.mean(), scores.std()))
 print("Total processing time: {:.2f} minutes".format((time.time()-start_time)/60))
 
+#%% ------------------------------------------------------------------------
+from sklearn.metrics import confusion_matrix
 
-#confusion_matrix(y_test, y_pred)
+confusion_matrix(y_test, y_pred)
+#      [[1101,    0,    4,    0,    4,   38],
+#       [   0, 1020,   54,    0,    2,    3],
+#       [   1,   46, 2174,    0,   31,   40],
+#       [   6,    0,    1,  106,    0,   58],
+#       [   0,    1,   83,    0,  721,   35],
+#       [   5,    1,    6,    4,   22, 3688]]
+
+# "Jogging"  : 0, 
+# "LyingDown": 1,
+# "Sitting"  : 2,
+# "Stairs"   : 3,
+# "Standing" : 4,
+# "Walking"  : 5
 
 
 
